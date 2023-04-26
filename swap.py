7ceef50b328083482b8e4d7d039309d82a27e95c
kkv2000 <2000kkv@gmail.com>
2023-04-26 09:58:18 +0530

    Letter Case Swapping

---
+++ swap.py
@@ -0,0 +1,23 @@
+k = ""
+text = input("Enter the text :")
+for i in text:
+    if i.islower():
+        k+=i.upper()
+    else:
+        k+=i.lower()
+print (k)
+
+
+def swapping(x):
+    k = ""
+    for i in text:
+        if i.islower():
+            k += i.upper()
+        else:
+            k += i.lower()
+    print(k)
+text = input("Enter the text :")
+swapping(text)
+
+
+
