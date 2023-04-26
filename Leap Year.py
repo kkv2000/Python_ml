2a0106be09cabc031837921067198037853c8176
kkv2000 <2000kkv@gmail.com>
2023-04-26 10:19:48 +0530

    Leap Year

--- Leap Year.py
+++ Leap Year.py
@@ -2,7 +2,7 @@ year = int (input("Enter a year : "))
 if year % 4 != 0:
     print ("Not a leap year")
 elif year % 4 == 0:
-    if year % 100 == 0 & year % 400 == 0:
+    if year % 100 == 0 and year % 400 == 0:
         print ("Leap year")
     else :
         print ("Not a leap year")
