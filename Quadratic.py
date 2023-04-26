8e261a0dcbcddbb42fc046c1bd859c5d59b6a5cc
kkv2000 <2000kkv@gmail.com>
2023-04-26 09:49:40 +0530

    Odd or Even

---
+++ Quadratic.py
@@ -0,0 +1,10 @@
+a = int(input("Enter the value of a"))
+b = int(input("Enter the value of b"))
+c = int(input("Enter the value of c"))
+print(a,b,c)
+k = (b**2)-(4*a*c)
+d = k**0.5
+x1 = (-b+d)/(2*a)
+x2 = (-b-d)/(2*a)
+print(x1,x2)
+
