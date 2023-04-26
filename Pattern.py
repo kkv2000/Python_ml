40e4e6e2172e5d39d1e67d02fbad3636d5730518
kkv2000 <2000kkv@gmail.com>
2023-04-26 11:34:54 +0530

    Pattern printing

--- Pattern.py
+++ Pattern.py
@@ -1,5 +1,54 @@
 n=int(input("Enter a number : "))
 for i in range (n):
     for j in range (i):
-        print(i,end ="")
+        print(1,end =" ")
+    print()
+
+n=int(input("Enter a number : "))
+for i in range (n):
+    for j in range (i):
+        print(j+1,end =" ")
+    print()
+
+n=int(input("Enter a number : "))
+for i in range (n):
+    for j in range (i):
+        print("*",end =" ")
+    print()
+
+n=int(input("Enter a number : "))
+for i in range (1,n):
+    for j in range (5-i):
+        print(i,end =" ")
+    print()
+
+n = int(input("Enter a number : "))
+for i in range(1, n):
+    for j in range(i,n):
+        print(i, end=" ")
+    print()
+
+n = int(input("Enter a number : "))
+for i in range(1, n):
+    for j in range(1,n):
+        print("*", end=" ")
+    print()
+
+
+for i in range(1, 6):
+    for j in range(1,6):
+        if j == 1 or i == 6 or i == j:
+            print("*", end=" ")
+        else:
+            print(" ",end=" ")
+    print()
+
+
+n = int(input("Enter a number : "))
+for i in range(1, n):
+    for j in range(1,n):
+        if j == 1 or i == n-1 or i == j:
+            print("*", end=" ")
+        else:
+            print(" ",end=" ")
     print()
